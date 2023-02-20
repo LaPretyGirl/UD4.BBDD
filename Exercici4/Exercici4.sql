f78abab7572fe9374d21946e838ff7f480080e5b
Aroa <apons20484@iesjoanramis.org>
2023-02-20 10:40:47 +0100

    Adjunt els exercicis

--- Exercici4/Exercici4.sql
+++ Exercici4/Exercici4.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema Exercici4
+-- Schema mydb
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema Exercici4
+-- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Exercici4` ;
-USE `Exercici4` ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `Exercici4`.`Client`
+-- Table `mydb`.`Client`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Exercici4`.`Client` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Client` (
   `DNI` INT NOT NULL,
   `Nom` VARCHAR(45) NOT NULL,
   `Cognoms` VARCHAR(45) NOT NULL,
@@ -30,39 +30,36 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Exercici4`.`Venedor`
+-- Table `mydb`.`Tipus polissa`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Exercici4`.`Venedor` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Tipus polissa` (
+  `ID` INT NOT NULL,
+  `Nom` VARCHAR(45) NOT NULL,
+  `Descripció` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Venedor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Venedor` (
   `DNI` INT NOT NULL,
   `Nom` VARCHAR(45) NOT NULL,
   `Cognoms` VARCHAR(45) NOT NULL,
   `Adreça` VARCHAR(45) NOT NULL,
   `Mòbil` VARCHAR(45) NOT NULL,
-  `Descripció` VARCHAR(45) NOT NULL,
+  `Descripció` VARCHAR(45) NULL,
   PRIMARY KEY (`DNI`),
   UNIQUE INDEX `DNI_UNIQUE` (`DNI` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Exercici4`.`Venedor`
+-- Table `mydb`.`Polissa`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Exercici4`.`Venedor` (
-  `DNI` INT NOT NULL,
-  `Nom` VARCHAR(45) NOT NULL,
-  `Cognoms` VARCHAR(45) NOT NULL,
-  `Adreça` VARCHAR(45) NOT NULL,
-  `Mòbil` VARCHAR(45) NOT NULL,
-  `Descripció` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`DNI`),
-  UNIQUE INDEX `DNI_UNIQUE` (`DNI` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Exercici4`.`Polissa`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Exercici4`.`Polissa` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Polissa` (
   `NumPolissa` INT NOT NULL,
   `Import` VARCHAR(45) NOT NULL,
   `DataInici` VARCHAR(45) NOT NULL,
@@ -78,17 +75,17 @@ CREATE TABLE IF NOT EXISTS `Exercici4`.`Polissa` (
   INDEX `fk_Polissa_Venedor2_idx` (`Venedor_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Polissa_Client`
     FOREIGN KEY (`Client_DNI`)
-    REFERENCES `Exercici4`.`Client` (`DNI`)
+    REFERENCES `mydb`.`Client` (`DNI`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Polissa_Venedor1`
     FOREIGN KEY (`Venedor_DNI`)
-    REFERENCES `Exercici4`.`Venedor` (`DNI`)
+    REFERENCES `mydb`.`Venedor` (`DNI`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Polissa_Venedor2`
     FOREIGN KEY (`Venedor_ID`)
-    REFERENCES `Exercici4`.`Venedor` (`ID`)
+    REFERENCES `mydb`.`Tipus polissa` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
